67b5aec24e4793f6426db8da15f6ce59c451e36b
Vinaikumarp <vinaypamarlaok@gmail.com>
2026-09-07 11:30:55 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
   public static void main(String[] args) {
       System.out.println("Hello, I am learning DevOps!");
-      System.out.println("Hello, I am learning DevSecOps!");
-      System.out.println("Hello, I am learning AWS!");
+      
   }
 }
