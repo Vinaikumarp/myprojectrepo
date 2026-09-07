624b39cd3499604b23ff7bf3687afabd299a57ab
Vinaikumarp <vinaypamarlaok@gmail.com>
2026-09-07 06:42:44 +0530

    Add additional print statements for learning topics

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
       System.out.println("Hello, I am learning DevOps!");
+      System.out.println("Hello, I am learning DevSecOps!");
+      System.out.println("Hello, I am learning AWS!");
   }
 }
